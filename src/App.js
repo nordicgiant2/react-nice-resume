29cf627e07053bf503d8035eef7846ff4c91c6ac
Chengelle91 <Chengelle91@gmail.com>
2023-07-18 21:41:11 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ class App extends Component {
         <Logo />
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
-        {/* <Resume data={this.state.resumeData.resume} /> */}
         <Portfolio data={this.state.resumeData.portfolio} />
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
